66245966f845b84ec712320465e0330aba3c850b
Ian Hollands <ihollands@outlook.com>
2016-10-26 20:34:32 -0400

    not complete - unable to fix problems with instructor oversight - no idea

--- js/app.js
+++ js/app.js
@@ -1,5 +1,26 @@
-"use strict";
 
-(function(){
-  angular.module("wdinstagram", [])
-})();
+  angular
+    .module("wdinstagram", [
+      "ui.router"
+    ])
+    .config([
+      "$stateProvider",
+      Router
+    ])
+    .controller("InstaIndexController", [
+      InstaIndexControllerFunction
+    ])
+
+function Router($stateProvider){
+  $stateProvider
+    .state("instaIndex", {
+      url: "/",
+      templateUrl: "js/ng-views/index.html",
+      controller: "InstaIndexController",
+      controllerAs: "vm"
+    })
+}
+
+function InstaIndexControllerFunction(){
+  this.posts = [{title: "Hey Show Up"}]
+}
